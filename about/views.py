434b40ff3f8bc147aafc388c07df75ddb605cbf6
ShuaybAi <aijaz.shuayb@gmail.com>
2025-09-21 20:02:04 +0100

    add comment CRUD

--- about/views.py
+++ about/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 def about_me(request):
@@ -8,8 +10,19 @@ def about_me(request):
     """
     about = About.objects.order_by('-updated_on').first()
 
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
+
+    collaborate_form = CollaborateForm()
+
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about, "collaborate_form": collaborate_form},
     )
